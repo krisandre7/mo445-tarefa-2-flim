53e25a80cee7b96307a13b4cf3a9cf83ca14ab9a
shen-n <n.sbaldon@gmail.com>
2025-12-07 20:48:23 -0300

    final files

--- scripts/arcw_id_exp.sh
+++
@@ -1,7 +0,0 @@
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=0
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=1
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=2
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=3
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=4
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=5
-python src/main.py fit -c configs/data/schisto.yaml -c configs/model/schisto_segmentation.yaml --model.init_args.arcw_id=6
--- src/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.modules.mnist_module import SimpleNetModule
-from src.modules.schisto_segmentation import SchistoSegmentationModule
-__all__ = [
-    "SimpleNetModule",
-    "SchistoSegmentationModule",
-]
